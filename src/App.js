7b5877a2828f423d04742b0fc3d62e2ec704ae3f
DOH LAH nundo christian <150492813+christiandoh@users.noreply.github.com>
2026-01-29 19:09:06 +0000

    Change displayed text in App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
                 repeat: Infinity,
               }}
             >
-              <span>Doh Lah nundoh Christian</span>
+              <span>Demeures Benis!!🙏</span>
               <span className="loader-dot">.</span>
             </motion.div>
           </motion.div>
